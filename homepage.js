3e7fc286915c8425919b40fbab4bd0bc992bb977
damola <egbebitimi121@gmail.com>
2022-03-19 23:51:04 -0800

    not done yet

--- homepage.js
+++ homepage.js
@@ -1,47 +1,61 @@
-
-
-
-
-
-
-
-let leftButton = document.getElementById("nav-left")
-let rightButton = document.getElementById("nav-right")
+let navLeftBtn = document.getElementById("nav-left")
+let navRightBtn = document.getElementById("nav-right")
 let slideShow  = document.getElementById("slideshow")
-
+let mobile_menu = document.getElementById('mobile-menu')
+let mobile_menu_options = document.getElementById('menu-options')
+let closeMenuBtn = document.getElementById('close-menu')
 let vel = 0
+
+function moveMenuOnscreen() {
+	mobile_menu_options.style.left = '0';
+}
+
+function returnMenuOffScreen() {
+	mobile_menu_options.style.left = '-100%';
+}
+
+window.addEventListener('resize', returnMenuOffScreen)
+closeMenuBtn.addEventListener('click', returnMenuOffScreen)
 
 function moveLeft() { //moves the slideshow left
 	if (vel != -900) { //keeps the slideshow pictures within the right edge of the container
+		if (navRightBtn.style.display == 'none') {
+			navRightBtn.style.display = 'inline'
+		}
 		increase_right()
 		vel -= 100
 		images = slideShow.querySelectorAll("img")
 		for (let i=0; i<images.length; i++) {
 			images[i].style.transform = `translateX(${vel}%)`
 		}
-	}   
+	}
+	if (vel == -900) {navLeftBtn.style.display = 'none'}  
 }
 
 function moveRight() { //moves the slideshow right
 	if (vel != 0) { //keeps the slideshow pictures within the left edge of the container
+		if (navLeftBtn.style.display == 'none') {
+			navLeftBtn.style.display = 'inline'
+		}
 		increase_left()
 		vel += 100
 		images = slideShow.querySelectorAll("img")
 		for (let i=0; i<images.length; i++) {
 			images[i].style.transform = `translateX(${vel}%)`
 		}
-	}
+	}if (vel == 0) {navRightBtn.style.display = 'none'}
 }
 
-leftButton.addEventListener("click", moveLeft)
-rightButton.addEventListener("click", moveRight)
+mobile_menu.addEventListener('click', moveMenuOnscreen)
+navLeftBtn.addEventListener("click", moveLeft)
+navRightBtn.addEventListener("click", moveRight)
 
 let lines = document.querySelectorAll(".underline")
 lines = Array.from(lines)
 let index = 0
 let width_l = 99.99 //the width of the element that will be decreased if left btn is clicked
 let width_r = 0 //the width of the element that will be decreased if right btn is clicked
-let cvl = [lines[0]]
+let currentlyVisibleLines = [lines[0]]
 let active_left = true
 let active_right = false
 
@@ -50,23 +64,22 @@ function increase_right() {
 		width_l -= 33.33 
 		width_r = 99.99-width_l
 	}
-	if (cvl.length == 1) {
-		cvl[0].style.width = `${width_l}%`
-		cvl.push(lines[index + 1])
-		cvl[1].style.width = `${width_r}%`
-	}else if (cvl.length > 1) {
-		cvl[0].style.width = `${width_l}%`
-		cvl[1].style.width = `${width_r}%`
+	if (currentlyVisibleLines.length == 1) {
+		currentlyVisibleLines[0].style.width = `${width_l}%`
+		currentlyVisibleLines.push(lines[index + 1])
+		currentlyVisibleLines[1].style.width = `${width_r}%`
+	}else if (currentlyVisibleLines.length > 1) {
+		currentlyVisibleLines[0].style.width = `${width_l}%`
+		currentlyVisibleLines[1].style.width = `${width_r}%`
 	}
 	if (width_l == 0) {
-		if (lines.indexOf(cvl[0]) == index) {
+		if (lines.indexOf(currentlyVisibleLines[0]) == index) {
 			index += 1
 		}
-		cvl.shift()
+		currentlyVisibleLines.shift()
 		width_l = 99.99
 		width_r = 99.99
 	}
-	console.log(cvl, index)
 }
 
 function increase_left() {
@@ -74,20 +87,20 @@ function increase_left() {
 		width_r -= 33.33 
 		width_l = 99.99-width_r
 	}
-	if (cvl.length == 1) {
-		cvl[0].style.width = `${width_r}%`
-		cvl.unshift(lines[index - 1])
-		cvl[0].style.width = `${width_l}%`
-	}else if (cvl.length > 1) {
-		cvl[1].style.width = `${width_r}%`
-		cvl[0].style.width = `${width_l}%`
+	if (currentlyVisibleLines.length == 1) {
+		currentlyVisibleLines[0].style.width = `${width_r}%`
+		currentlyVisibleLines.unshift(lines[index - 1])
+		currentlyVisibleLines[0].style.width = `${width_l}%`
+	}else if (currentlyVisibleLines.length > 1) {
+		currentlyVisibleLines[1].style.width = `${width_r}%`
+		currentlyVisibleLines[0].style.width = `${width_l}%`
 	}
 	if (width_r == 0) {
-		if (lines.indexOf(cvl[1]) == index) {
+		if (lines.indexOf(currentlyVisibleLines[1]) == index) {
 			index -= 1
 		}
-		cvl.splice(1, 1)
+		currentlyVisibleLines.splice(1, 1)
 		width_r = 99.99
 		width_l = 99.99
-	}console.log(cvl, index)
+	}
 }
